d20081a118e45f793bb857736bb91bed03c51dc8
Austin Hutmacher <hutmacheraustin@gmail.com>
2023-11-20 17:18:35 -0700

    fixed error in nav fade js

--- js/navigation_fade.js
+++ js/navigation_fade.js
@@ -1,10 +1,16 @@
 function fadeButtons() {
   $(document).ready(function(){
+    
     $('nav a, footer a, button').mouseover(function(){
       $(this).fadeTo(150, .3);
     });
+    
     $('nav a, footer a, button').mouseout(function(){
       $(this).fadeTo(150, 1);
+    });
+
+    $('nav a, footer a, button').on('click', function(){
+      $('nav a, footer a, button').fadeTo(0, 1);
     });
   });
 };
